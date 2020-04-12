852bbcd29b980977ad8cea85346cde85d94a90e5
Rohit <rjha@ph.iitr.ac.in>
2020-04-12 20:49:12 +0530

    Revert last

--- django/core/management/commands/makemessages.py
+++ django/core/management/commands/makemessages.py
@@ -18,7 +18,7 @@ from django.utils.functional import cached_property
 from django.utils.jslex import prepare_js_for_gettext
 from django.utils.regex_helper import _lazy_re_compile
 from django.utils.text import get_text_list
-from django.utils.translation import templatize, to_locale
+from django.utils.translation import templatize
 
 plural_forms_re = _lazy_re_compile(r'^(?P<value>"Plural-Forms.+?\\n")\s*$', re.MULTILINE | re.DOTALL)
 STATUS_OK = 0
@@ -166,13 +166,6 @@ def normalize_eols(raw_contents):
         lines_list.append('')
     return '\n'.join(lines_list)
 
-def normalize_locale(locale_str):
-    """
-    Normalize locale string to use a single locale format.
-    Eg. (zh-cn, zh_cn, zh_CN, ZH-CN) all map to zh_CN
-    """
-    language = locale_str.lower().replace('_', '-')
-    return to_locale(language)
 
 def write_pot_file(potfile, msgs):
     """
